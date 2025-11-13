a4f6341989080a53e57499cf8ab539041368013b
ennmax <shayaan.qureshi@ontariotechu.net>
2025-11-13 01:32:39 -0500

    Added Express Routes for Home, About Me, Projects, and Contact Me pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
