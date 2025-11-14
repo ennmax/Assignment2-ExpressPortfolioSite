b99c3c90073d2f75b511e8b00125aa0260243eb2
ennmax <shayaan.qureshi@ontariotechu.net>
2025-11-13 23:06:54 -0500

    Added content to all pages, added links in footer, wrote comments, organized code

--- public/Script/app.js
+++ public/Script/app.js
@@ -1,9 +1,13 @@
 // IIFE
 // Immediately Invoked Function Expression
+// From Lecture 7
 
 (function() {
     function start() {
+        // Below message appears in the browser's console when page finishes loading
         console.log("App started")
     }
+    
+    // Register start function so it runs after all page assets have loaded
     window.addEventListener("load", start);
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,32 @@
+// Defines the main routes for the website
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET default route / - treated as Home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET home page (under /home). */
+// GET /home - Home page
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET About Me page. */
+// GET /about - About Me page
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About Me' });
 });
 
-/* GET Projects page. */
+// GET /projects - Projects page
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects' });
 });
 
-/* GET Contact Me page. */
+// GET /contact - Contact Me page
 router.get('/contact', function(req, res, next) {
   res.render('index', { title: 'Contact Me' });
 });
 
+// Export router for use in app.js
 module.exports = router;
